a28f6dd865d09cc416088319c5e50c22b0c6849a
cristian-luca <prof_cristianluca@hotmail.com>
2023-10-17 21:44:35 +0300

    add Webdriver driver

---
+++ src/main/java/driverFactory/DriverFactory.java
@@ -0,0 +1,26 @@
+package driverFactory;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class DriverFactory {
+    private static WebDriver driver = null;
+    public static WebDriver setUp() {
+
+        if (driver == null) {
+            System.setProperty("webdriver.chrome.driver", getChromeDriverPath() + "\\drivers\\chromedriver.exe");
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--remote-allow-origins=*");
+            driver = new ChromeDriver();
+            driver.manage().window().setSize(new Dimension(1920, 1080));
+            driver.manage().deleteAllCookies();
+        }
+        return driver;
+    }
+
+    private static String getChromeDriverPath() {
+        return System.getProperty("user.dir");
+    }
+}
