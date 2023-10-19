0ea8711ce8ae325ef10712e08b16a3523b6d9664
cristian-luca <prof_cristianluca@hotmail.com>
2023-10-19 21:12:23 +0300

    metodele clear, click, sendKeys

--- src/test/java/tests/Test.java
+++ src/test/java/tests/Test.java
@@ -1,13 +1,32 @@
 package tests;
 
 import driverFactory.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 public class Test {
-    public static void main(String[] args) {
-        DriverFactory.setUp().get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    static WebDriver driver = DriverFactory.setUp();
+    public static void main(String[] args) throws InterruptedException {
+        DriverFactory.setUp().get("https://www.saucedemo.com/");
         //s-a deschis o instanta de chrome; in consola de la IJ apare mesajul ChromeDriver was started successfully
         System.out.println(DriverFactory.setUp().getTitle());
         //retrieve the title of the current webpage---> ca o masura suplimentara ca Selenium a lucrat conform asteptarii
-        DriverFactory.setUp().quit(); //terminate the WebDriver session.
+        Thread.sleep(5000);
+        driver.findElement(By.id("user-name")).clear();
+        driver.findElement(By.id("user-name")).click();
+        Thread.sleep(5000);
+
+        WebElement username = driver.findElement(By.id("user-name"));
+        username.sendKeys("standard_user");
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).click();
+        Thread.sleep(5000);
+        WebElement pwd = driver.findElement(By.id("password"));
+        pwd.sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        Assert.assertEquals("https://www.saucedemo.com/", driver.getCurrentUrl());
+        //DriverFactory.setUp().quit(); //terminate the WebDriver session.
     }
 }
