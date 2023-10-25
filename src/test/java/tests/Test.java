8cfe083aa84359930aa65eb3a37970880ae64fda
cristian-luca <prof_cristianluca@hotmail.com>
2023-10-25 19:41:54 +0300

    tema refacuta in mod structurat

--- src/test/java/tests/Test.java
+++ src/test/java/tests/Test.java
@@ -17,9 +17,36 @@ public class Test {
 
         driver.get("https://www.saucedemo.com/v1/index.html");
         //s-a deschis o instanta de chrome; in consola de la IJ apare mesajul ChromeDriver was started successfully
-        System.out.println(DriverFactory.setUp().getTitle());
-        //retrieve the title of the current webpage---> ca o masura suplimentara ca Selenium a lucrat conform asteptarii
+
+        System.out.println(driver.getTitle());
+        //retrieve the title of the current webpage---> ca o masura suplimentara ca Selenium a lucrat conform asteptarilor
+
         Thread.sleep(3000);
+
+        loginPage(driver);
+
+        Assert.assertEquals("https://www.saucedemo.com/v1/inventory.html", driver.getCurrentUrl());
+
+        String[] numeProduseDorite = {"Sauce Labs Backpack", "Sauce Labs Bike Light", "Sauce Labs Fleece Jacket", "Sauce Labs Onesie"};
+        List<WebElement> listaProduse = driver.findElements(By.xpath("//div[@class='inventory_item']//div[@class='inventory_item_label']//div[@class='inventory_item_name']"));
+        addProduse(driver, numeProduseDorite, listaProduse);
+
+        driver.findElement(By.xpath("//span[@class='fa-layers-counter shopping_cart_badge']")).click();
+        Thread.sleep(3000);
+        Assert.assertEquals("https://www.saucedemo.com/v1/cart.html", driver.getCurrentUrl());
+
+        List<WebElement> listaProduseSelectata = driver.findElements(By.xpath("//div[@class='cart_list']//div[@class='cart_item']//button"));
+        List<WebElement> listaProduseRamasa = driver.findElements(By.xpath("//div[@class='cart_list']//div[@class='cart_item']//button"));
+        int indexRandom = new Random().nextInt(listaProduseSelectata.size());
+
+        produsRandomRemoved(driver, listaProduseSelectata, indexRandom);
+        Thread.sleep(5000);
+        validareFunctionalitate(listaProduseSelectata, listaProduseRamasa);
+
+        driver.quit();
+    }
+
+    public static void loginPage(WebDriver driver) throws InterruptedException {
         driver.findElement(By.id("user-name")).clear();
         driver.findElement(By.id("user-name")).click();
         Thread.sleep(3000);
@@ -31,54 +58,39 @@ public class Test {
         WebElement pwd = driver.findElement(By.id("password"));
         pwd.sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
-        Assert.assertEquals("https://www.saucedemo.com/v1/inventory.html", driver.getCurrentUrl());
+    }
 
-        String[] numeProduse = {"Backpack", "Light", "Bolt", "Onesie"}; //am ales 4 produse care contin aceste nume
+    public static void produsRandomRemoved(WebDriver driver, List<WebElement> lista, int index) throws InterruptedException {
+        WebElement randomButonRemove = lista.get(index);
+        randomButonRemove.click();
+        Thread.sleep(3000);
+    }
 
+    public static void validareFunctionalitate(List<WebElement> listaPS, List<WebElement> listaPR) {
+        if (listaPS.size() != listaPR.size()) {
+            System.out.println("Acesta functionalitate a decurs asa cum ne-am asteptat!");
+        } else {
+            System.out.println("Ceva nu a mers bine. De verificat!");
+        }
+    }
+
+    public static void addProduse(WebDriver driver, String[] numeProduse, List<WebElement> listaProduse) throws InterruptedException {
         List<String> numeProduseAsList = Arrays.asList(numeProduse);
         Thread.sleep(3000);
-        List<WebElement> listaProduse = driver.findElements(By.xpath("//div[@class='inventory_item']//div[@class='inventory_item_label']//div[@class='inventory_item_name']"));
+
         int count = 0;
         for (int i = 0; i < listaProduse.size(); i++) {
             String numeProdus = listaProduse.get(i).getText();
-            System.out.println(listaProduse.get(i).getText());
+            //System.out.println(numeProdus); a fost folosit pentru verificarea textului
             if (numeProduseAsList.contains(listaProduse.get(i).getText())) {
                 count++;
-                Thread.sleep(5000);
+                Thread.sleep(1000);
                 WebElement elem = driver.findElements(By.xpath("//div[@class='inventory_item']//button")).get(i);
                 elem.click();
-                /*
-                nu imi da click pe niciunul din cele 4 butoane pentru produsele selectate din lista ca sa pot
-                sa le comand pe site. De ce oare? Mersi!
-                 */
                 if (count == numeProduse.length) {
                     break;
                 }
             }
         }
-
-//        driver.findElement(By.xpath("(//div[@class='inventory_item']//button)[1]")).click();
-//        driver.findElement(By.xpath("(//div[@class='inventory_item']//button)[2]")).click();
-//        driver.findElement(By.xpath("(//div[@class='inventory_item']//button)[3]")).click();
-//        driver.findElement(By.xpath("(//div[@class='inventory_item']//button)[4]")).click();
-//        driver.findElement(By.xpath("(//div[@class='inventory_item']//button)[5]")).click();
-//        driver.findElement(By.xpath("(//div[@class='inventory_item']//button)[6]")).click();
-//        driver.findElement(By.xpath("//span[@class='fa-layers-counter shopping_cart_badge']")).click();
-//        Thread.sleep(3000);
-//        Assert.assertEquals("https://www.saucedemo.com/v1/cart.html", driver.getCurrentUrl());
-//
-//        List<WebElement> listaProduseSelectata = driver.findElements(By.xpath("//div[@class='cart_list']//div[@class='cart_item']//button"));
-//        int index = new Random().nextInt(6) + 1;
-//        WebElement randomButonRemove = listaProduseSelectata.get(index);
-//        randomButonRemove.click();
-//        Thread.sleep(3000);
-//
-//        List<WebElement> listaProduseRamasa = driver.findElements(By.xpath("//div[@class='cart_list']//div[@class='cart_item']//button"));
-//
-//        if (listaProduseSelectata.size() != listaProduseRamasa.size()) {
-//            System.out.println("Acesta functionalitate a decurs asa cum ne-am asteptat!");
-//        }
-
-        //driver.quit(); //terminate the WebDriver session.
     }
 }
